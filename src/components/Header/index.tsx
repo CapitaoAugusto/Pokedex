17e9c3bf8cfcc112184fbbebd6f571ece0be412b
/Otávio Augusto <vedanajazz@gmail.com>
2023-05-23 16:18:08 -0300

    feature-HomePageMobile
    HomePage completa incluindo resposividade para Tablets e Mobile com a adição do link para a API do pokedex.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
 import Logo from '../../assets/Logo.svg'
+import BotaoHamburger from '../../assets/BotaoHamburger.svg'
 import styles from "./index.module.css"
+import { HeaderMobile } from '../HeaderMobile';
 
 export const Header = () => {
+
+    const { tamanhoWidth: width } = window;// guardar tamanho da tela
+    const [headerMobile, setHeaderMobile] = useState(false);
+    const viewHeaderMobile = () => setHeaderMobile(!headerMobile);
+    const menuBlock = useRef<HTMLInputElement | null>(null);
+
+
+    useEffect(() => {
+        const controle = (e: any) => {
+            if (!menuBlock.current?.contains(e.target)) {
+                setHeaderMobile(false);
+            }
+        }
+        document.addEventListener("mousedown", controle)
+        return () => {
+            document.removeEventListener("mousedown", controle);
+        }
+    });
+
+
     return (
-        <header className={styles.header}>
+        <header className={styles.header} ref={menuBlock}>
             <a>
                 <img className={styles.imgLogo} src={Logo} alt="Logo do Pokedex" />
             </a>
+            <img
+                    onClick={viewHeaderMobile}
+                    className={styles.isHidden}
+                    src={BotaoHamburger}
+                    alt="Botão hamburger mobile"
+                />
+                {headerMobile && <HeaderMobile />}
             <section className={styles.section_buttons}>
                 <a>
                     <button className={styles.button}>Home</button>
@@ -17,7 +47,7 @@ export const Header = () => {
                 <a>
                     <button className={styles.button}>Legendaries</button>
                 </a>
-                <a>
+                <a  href='https://pokeapi.co/'>
                     <button className={styles.button}>Documentation</button>
                 </a>
             </section>
