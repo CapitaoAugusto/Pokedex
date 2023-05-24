86b4436bb5bfd7b32c8603b265a5d176e4a6c87a
/Otávio Augusto <vedanajazz@gmail.com>
2023-05-24 15:56:14 -0300

    feature-Page404NotFound
    Pagina de Error 404 completa, incluindo react router e resposividade para dispositivos como tablet e mobile.

--- src/components/HeaderMobile/index.tsx
+++ src/components/HeaderMobile/index.tsx
@@ -1,18 +1,19 @@
 import styles from './index.module.css';
 import Logo from "../../assets/Logo.svg"
+import { Link } from 'react-router-dom';
 
 export const HeaderMobile = () => {
 
     return (
 
         <header className={styles.headerContainerMobile}>
-            <a>
-                <img className={styles.imgLogoMobile} src={Logo} alt="Logo do Pokedex" />
-            </a>
+            <Link to="/"><img className={styles.imgLogoMobile} src={Logo} alt="Logo do Pokedex" /></Link>
+
+
             <section className={styles.section_buttonsMobile}>
-                <a>
+                <Link to="/">
                     <button className={styles.buttonMobile}>Home</button>
-                </a>
+                </Link>
                 <a>
                     <button className={styles.buttonMobile}>Pokédex</button>
                 </a>
